0c24456b3a5eb81b160824866a9c4e19a3b79cb1
ImperioKDB <kingbrite123@gmail.com>
2026-07-18 18:31:17 +0100

    Day 7: badge URL updated to new uploaded file

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -22,7 +22,7 @@ export function SiteNav() {
           <Link href="/" className="flex items-center">
             <div className="relative w-11 h-11 shrink-0">
               <Image
-                src="https://gwxiwvpqmcrkrdiqaokp.supabase.co/storage/v1/object/public/legacy-book-assets/site/badge.png"
+                src="https://gwxiwvpqmcrkrdiqaokp.supabase.co/storage/v1/object/public/legacy-book-assets/site/file_0000000054088246b79aebd9c67c00f7.png"
                 alt="TACSFON"
                 fill
                 className="object-contain"
